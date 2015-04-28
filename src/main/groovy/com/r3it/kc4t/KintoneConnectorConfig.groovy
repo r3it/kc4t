8c01d4d3a8ab4cad6c07e7f32c85f59809385626
K Nishijima <nishijima@r3it.com>
2015-04-28 10:52:13 +0900

    初期ビルドファイルとSpec追加

---
+++ src/main/groovy/com/r3it/kc4t/KintoneConnectorConfig.groovy
@@ -0,0 +1,12 @@
+package com.r3it.kc4t
+
+class KintoneConnectorConfig {
+  def jdbcUrl = ""
+  def tablePrefix = ""
+  def jobStatusReportTable = ""
+  def saveTmpTable = true
+
+  def apiToken = ""
+  def subDomain = ""
+  def appId = ""
+}
