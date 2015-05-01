e31dcdd470a4f211a469de3a147e0a8755356d4c
K Nishijima <nishijima@r3it.com>
2015-05-01 14:48:17 +0900

    ExportRecordSet.addResultSet実装

--- src/test/groovy/com/r3it/kc4t/ExportRecordSetSpec.groovy
+++ src/test/groovy/com/r3it/kc4t/ExportRecordSetSpec.groovy
@@ -19,7 +19,7 @@ class ExportRecordSetSpec extends Specification {
 
         when:
         rs.next()
-        exportRecordSet.start()
+        exportRecordSet.next()
         exportRecordSet.addResultSet(rs, FieldType.SINGLE_LINE_TEXT, 'userName')
         exportRecordSet.addResultSet(rs, FieldType.NUMBER, 'userId')
 
@@ -57,12 +57,12 @@ class ExportRecordSetSpec extends Specification {
 
         when:
         rs.next()
-        exportRecordSet.start()
+        exportRecordSet.next()
         exportRecordSet.addResultSet(rs, FieldType.CALC, 'total')
         exportRecordSet.addResultSet(rs, FieldType.MULTI_LINE_TEXT, 'msg')
         exportRecordSet.addResultSet(rs, FieldType.CHECK_BOX, 'likes')
         rs.next()
-        exportRecordSet.start()
+        exportRecordSet.next()
         exportRecordSet.addResultSet(rs, FieldType.CALC, 'total')
         exportRecordSet.addResultSet(rs, FieldType.MULTI_LINE_TEXT, 'msg')
         exportRecordSet.addResultSet(rs, FieldType.CHECK_BOX, 'likes')
@@ -84,5 +84,58 @@ class ExportRecordSetSpec extends Specification {
         exportRecordSet.getRecords().get(1).colValues.get(0) == expected[0] + "2"
         exportRecordSet.getRecords().get(1).colValues.get(1) == expected[1] + "2"
         exportRecordSet.getRecords().get(1).colValues.get(2) == expected[2] + "2"
+    }
+
+    def "addResultSet-subtable"() {
+        setup:
+        def fk = 12345l
+        def exportRecordSet = new ExportRecordSet()
+        def rs = new ResultSet(null)
+        def record = new Record()
+        record.setId(fk)
+        record.setString("userName", "test taro")
+        rs.add(record)
+
+        def child1Rs = new ResultSet(null)
+        def child1Record = new Record()
+        child1Record.setId(fk)
+        child1Record.setString("childName", "child taro")
+        child1Rs.add(child1Record)
+        def child2Rs = new ResultSet(null)
+        def child2Record = new Record()
+        child2Record.setId(fk)
+        child2Record.setString("childName", "child jiro")
+        child2Rs.add(child2Record)
+
+        when:
+        rs.next()
+        exportRecordSet.next()
+        exportRecordSet.addResultSet(rs, FieldType.__ID__, '$id')
+        exportRecordSet.addResultSet(rs, FieldType.SINGLE_LINE_TEXT, 'userName')
+        exportRecordSet.addResultSet(rs, FieldType.SUBTABLE, 'children')
+
+        child1Rs.next()
+        child2Rs.next()
+        exportRecordSet.getSubTable(fk +'_children').addResultSet(child1Rs, FieldType.SINGLE_LINE_TEXT, 'childName')
+        exportRecordSet.getSubTable(fk +'_children').nextSubTableRecord(fk)
+        exportRecordSet.getSubTable(fk +'_children').addResultSet(child2Rs, FieldType.SINGLE_LINE_TEXT, 'childName')
+
+        then:
+        exportRecordSet.getRecords().size() == 1
+        exportRecordSet.getRecords().get(0).colNames.size() == 3
+        exportRecordSet.getRecords().get(0).colNames.get(0) == '$id'
+        exportRecordSet.getRecords().get(0).colNames.get(1) == "userName"
+        exportRecordSet.getRecords().get(0).colNames.get(2) == "children"
+        exportRecordSet.getRecords().get(0).colValues.size() == 2 // subTableは値を持たないから
+        exportRecordSet.getRecords().get(0).colValues.get(0) == fk.toString()
+        exportRecordSet.getRecords().get(0).colValues.get(1) == "test taro"
+
+        exportRecordSet.getSubTable(fk +'_children').records.size() == 2
+        exportRecordSet.getSubTable(fk +'_children').records.get(0).foreignKey == fk
+        exportRecordSet.getSubTable(fk +'_children').records.get(0).colNames.get(0) == "childName"
+        exportRecordSet.getSubTable(fk +'_children').records.get(0).colValues.get(0) == "child taro"
+        exportRecordSet.getSubTable(fk +'_children').records.get(1).foreignKey == fk
+        exportRecordSet.getSubTable(fk +'_children').records.get(1).colNames.get(0) == "childName"
+        exportRecordSet.getSubTable(fk +'_children').records.get(1).colValues.get(0) == "child jiro"
     }
 }
