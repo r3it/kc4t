610dc5ef66cad03c81cc0e8cae6213170ad486f1
K Nishijima <nishijima@r3it.com>
2016-03-18 10:44:59 +0900

    appIdをContext経由で指定しても問題ないように修正

--- src/main/groovy/com/r3it/kc4t/KintoneConnectorConfig.groovy
+++ src/main/groovy/com/r3it/kc4t/KintoneConnectorConfig.groovy
@@ -12,8 +12,20 @@ class KintoneConnectorConfig {
 
     def apiToken
     def subDomain
-    def Long appId
-    def Long guestSpaceId
+    def appId
+    def Long getAppId() {
+        return Long.parseLong(appId)
+    }
+
+    def guestSpaceId
+    def Long getGuestSpaceId() {
+        try {
+            return Long.parseLong(guestSpaceId)
+        } catch (Throwable t) {
+            return 0
+        }
+    }
+
     def orderByField = 'レコード番号'
     def useRevision = true
 
